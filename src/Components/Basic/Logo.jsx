0a3bf06576277a20d03756f8e527b2334536aea9
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2023-10-16 10:47:11 -0300

    add alt to logo image

--- src/Components/Basic/Logo.jsx
+++ src/Components/Basic/Logo.jsx
@@ -3,7 +3,7 @@ import Namicz from '../../assets/namicz-Icon.svg';
 const Logo = () => {
   return (
     <div className="my-auto w-fit font-semibold text-4xl">
-        <a href="#"className="flex content"><img src={Namicz} className="w-[40px]"/>
+        <a href="#"className="flex content"><img src={Namicz} className="w-[40px]" alt="Namicz"/>
         <h1 className="my-auto ml-0.5 lg:block text-black">AMICZ</h1></a>
     </div>
   );
