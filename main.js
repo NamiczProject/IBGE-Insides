bf2c8d9396bf0213fe3d8ff0487cfbd4ff3fb1c9
Victor Hugo <d.hugo@aluno.ifsp.edu.br>
2024-10-06 17:05:56 -0300

    namicz with electron

---
+++ main.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from "electron/main";
+import { join } from "node:path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"),
+    },
+  });
+
+  win.setIcon(join(__dirname, "/src/assets/namicz-Icon.png"));
+  win.loadURL("http://localhost:5173/");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
